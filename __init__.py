d7a38f1eca512d65278b714684ccc3ca0270ffbd
usuario <atorresh@gmail.com>
2020-04-18 18:47:24 +0200

    harcoded pdf file path

--- __init__.py
+++ __init__.py
@@ -4,12 +4,10 @@ from opsdroid.events import File
 
 class SendFileToTelegramSkill(Skill):
 
-    @match_regex(r'!file')
+    @match_regex(r'Please, send me a pdf file')
     async def sendDocument(self, message):
-
-        path= 'file.pdf'
+        path= '/file.pdf' # This is just for testing purposes, and it must point to a file in your file system. Not in the git repository.
 
         with open(path, 'rb') as file:
-            fileEvent = File(file_bytes= file, target= {'id': message.target}, mimetype= 'application/pdf')
-
+            fileEvent = File(file_bytes= file, mimetype= 'application/pdf', target= {'id': message.target})
             await message.respond( fileEvent )
